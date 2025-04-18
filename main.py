10bf3ae16510ed5d08fa2355c05721604f492fe5
Park Junsung <junsungpark774@gmail.com>
2025-04-18 16:08:32 +0900

    model router is added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 from log.logger import setup_logger
 from router.drive.drive_requestHandler import drive_handler
 from router.camera.camera_requestHandler import camera_handler
+from router.model.model_requestHandler import model_handler
 import uvicorn
 from dotenv import load_dotenv
 import os
@@ -19,6 +20,7 @@ logging_info = setup_logger('main', 'log_main.txt', logging.INFO)
 
 app.include_router(drive_handler)
 app.include_router(camera_handler)
+app.include_router(model_handler)
 
 if __name__ == "__main__":
     logging_info.info("FastAPI server starting the pi_drive_drive...")
